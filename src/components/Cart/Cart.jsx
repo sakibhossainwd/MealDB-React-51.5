7ce7c50a7ff53ff96b97f319ac955517e23348b6
Sakib Hossain <sakibhossainceo.com>
2023-11-07 23:37:17 +0600

    meal list added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,23 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = (props) => {
+const Cart = ({cart}) => {
+    // console.log(props.cart.strMeal)
+    // let quantity = 0;
+    let mealName = '';
+    for(const meal of cart){
+        // meal.quantity = meal.quantity || 1;
+        mealName = meal.strMeal
+    }
+
     return (
         <div className='cart-container'>
-            <h3>Total Food Items: {props.cart.length}</h3>
+            <h3>Total Food Items: {cart.length}</h3>
+            <div className="food-list">
+                <ol>
+                    <li>{mealName}</li>
+                </ol>
+            </div>
         </div>
     );
 };
