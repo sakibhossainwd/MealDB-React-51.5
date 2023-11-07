9eabc06d47f298c43c01ed809e705fc50ccc0416
Sakib Hossain <sakibhossainceo.com>
2023-11-07 09:27:10 +0600

    Header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header">
+                <div className="logo">
+                    <h2>MealBD</h2>
+                </div>
+                <div className="input">
+                    <input type="text" placeholder='search your food' />
+                    <button>Search</button>
+                </div>
+                <div className="info">
+                    <a href="#">Home</a>
+                    <a href="#">Our Meal</a>
+                    <a href="#">Offers</a>
+                    <a href="#">Contact</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
